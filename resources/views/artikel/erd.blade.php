a22fdc22675a0f1d907097c0b3dbdf78c72e4a36
HendrataSanber <hendrata.loekman@gmail.com>
2020-07-05 11:54:43 +0700

    Quiz 3

---
+++ resources/views/artikel/erd.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.master')
+
+@section('content')
+<img src="{{asset('image/ERD.png')}}"/>
+<br><br><br>
+<a href="/artikel" class="btn btn-sm btn-info">Go to artikel</a>
+@endsection
